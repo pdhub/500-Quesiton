7a5e631cdf2b4c2696f3ac18d9d339b391e3ca9f
Pritom Deb <pritom.deb@microfocus.com>
2017-12-28 20:03:12 +0530

    Convert Binary Tree to its sum tree
    http://www.techiedelight.com/inplace-convert-a-tree-sum-tree/

---
+++ src/main/java/binaryTree/ConvertBinaryTreeToSumTree.java
@@ -0,0 +1,24 @@
+package binaryTree;
+
+/**
+ * Created by PDeb on 12/28/2017.
+ */
+public class ConvertBinaryTreeToSumTree {
+
+    public int convertToSum(Node root)
+    {
+        if(root == null)
+            return 0;
+        int left = convertToSum(root.left);
+        int right = convertToSum(root.right);
+
+        int temp = root.data;
+        root.data = left+right;
+        return left+right+temp;
+    }
+
+    class Node{
+        int data;
+        Node left, right;
+    }
+}
