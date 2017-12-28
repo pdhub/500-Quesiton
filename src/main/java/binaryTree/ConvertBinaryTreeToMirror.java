a54ec9bcb38d4a3d3002065090add0444e808be1
Pritom Deb <pritom.deb@microfocus.com>
2017-12-28 21:08:10 +0530

    Convert given binary tree to its mirror
    http://www.techiedelight.com/convert-binary-tree-to-its-mirror/

---
+++ src/main/java/binaryTree/ConvertBinaryTreeToMirror.java
@@ -0,0 +1,17 @@
+package binaryTree;
+
+/**
+ * Created by PDeb on 12/28/2017.
+ */
+public class ConvertBinaryTreeToMirror {
+    public void convert(Node root){
+        if(root == null)
+            return;
+
+        convert(root.left);
+        convert(root.right);
+        Node temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+    }
+}
