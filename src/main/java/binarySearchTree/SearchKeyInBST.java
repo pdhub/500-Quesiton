592df41d9af3dc2056cf47711098c4cfda371002
Pritom Deb <pritom.deb@microfocus.com>
2017-12-31 19:22:31 +0530

    Search Key in BST(need review)

---
+++ src/main/java/binarySearchTree/SearchKeyInBST.java
@@ -0,0 +1,23 @@
+package binarySearchTree;
+
+import binaryTree.Node;
+
+/**
+ * Created by PDeb on 12/31/2017.
+ */
+public class SearchKeyInBST {
+
+    public boolean search(Node root, int key){
+        if (root == null)
+            return false;
+
+        if (root.key == key)
+            return true;
+
+        if(key < root.key)
+            return search(root.left, key);
+        else
+            return search(root.right, key);
+
+    }
+}
